bb97bde9c88075778e8d765267d2d0ccbded4be1
Markus Fisch <mf@markusfisch.de>
2017-09-06 23:29:25 +0200

    Simplified embedding and compressing sources

--- index.html.sh
+++ index.html.sh
@@ -1,50 +1,20 @@
 #!/usr/bin/env bash
-extract() {
-	local ECHO=0
+embed() {
 	while read -r
 	do
-		[[ $REPLY == *$1* ]] && {
-			ECHO=1
+		[[ $REPLY == '<script src="src.js"></script>' ]] && {
+			echo '<script>'
+			closurecompiler < src.js
+			echo '</script>'
+			continue
 		}
-		(( ECHO )) && {
-			# skip comments
-			REPLY=${REPLY%%//*}
-			# skip indent
-			REPLY=${REPLY##*$'\t'}
-			# skip empty lines
-			[ "$REPLY" ] || continue
-			echo "$REPLY"
-		}
-		[[ $REPLY == \</script* ]] && {
-			ECHO=0
-		}
+		# skip comments
+		REPLY=${REPLY%%//*}
+		# skip indent
+		REPLY=${REPLY##*$'\t'}
+		# skip empty lines
+		[ "$REPLY" ] || continue
+		echo "$REPLY"
 	done
 }
-cat << EOF
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8"/>
-<meta name="viewport" content="width=device-width"/>
-<title>js13k2017</title>
-<style>
-html, body {
-margin: 0; padding: 0;
-overflow: hidden;
-}
-canvas {
-position: fixed;
-width: 100%;
-height: 100%;
-}
-</style>
-</head>
-<body>
-<canvas id="Canvas">Sorry, this browser cannot render this content.</canvas>
-$(extract 'x-shader' < preview.html)
-<script>
-$(closurecompiler < src.js)
-</script>
-</body>
-</html>
-EOF
+embed < preview.html
